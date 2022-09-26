420a4ad3515f0626c1b9949277b5006dda411065
probablyliquid <111677910+probablyliquid@users.noreply.github.com>
2022-09-26 11:17:54 +0530

    Update V1.1
    - Added Dark Mode
    - Toggle for Dark Mode
    - Changes in Files (Resources)

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const checkbox = document.getElementById('checkbox')
+const qr = document.getElementById('code')
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+})
